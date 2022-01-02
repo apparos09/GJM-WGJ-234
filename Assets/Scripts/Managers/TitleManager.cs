754e6785d5bb331aa4b06b27780099eb7e802a3c
Roderick Montague <roderick.montague243@gmail.com>
2022-01-02 01:38:48 -0500

    Working on Menu
    Working on setting up the main menu.

--- Assets/Scripts/Managers/TitleManager.cs
+++ Assets/Scripts/Managers/TitleManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// the title manager.
 public class TitleManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    // toggles a game object 
+    public void ToggleGameObjectActive(GameObject go)
+    {
+        go.SetActive(!go.activeSelf);
     }
 
     // Update is called once per frame
